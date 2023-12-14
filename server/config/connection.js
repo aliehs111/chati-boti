2c9ac20b099eaa809562247b932f38374c508b0e
aliehs111 <128503077+aliehs111@users.noreply.github.com>
2023-12-14 10:25:24 -0500

    adds toastify, refactors enpoint paths, create new user works from fe to be to mdb

--- server/config/connection.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config(); // Load environment variables from .env file
-
-const mongoose = require('mongoose');
-
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social_network_api'
-);
-
-module.exports = mongoose.connection;
--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-// API routes
-
-router.use('/users', require('./userRoutes'));
-router.use('/thoughts', require('./thoughtRoutes'));
-
-
-module.exports = router;
